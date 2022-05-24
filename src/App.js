e5d7204cd011b8e56d736156eabd7c8430ac2c8a
Pablo Dellacassa <pdellacassa@gmail.com>
2022-05-24 18:33:23 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -4,32 +4,57 @@ import WeatherWidget from "./components/WeatherWidget";
 
 function App() {
   const [theme, setTheme] = useState("light");
+
   const [unit, setUnit] = useState("C");
 
-  const toggleTheme = (event) => {
+  const weather = [
+    {city: "Miami", icon:"🟡", celsius: 29},
+    {city: "Mexico City", icon:"⛈️", celsius: 17},
+    {city: "Berlin", icon:"🌥️", celsius: 20},
+    {city: "Barcelona", icon:"🌤️", celsius: 28} 
+  ];
+
+  /*   const handleSelect = (event) => {
     setTheme(event.target.value);
+  }; */
+
+  const handleSelect = (event) => {
+    setUnit(event.target.value);
   };
 
   return (
-    <div className={"App " + theme}>
-      <h1>React - state and events</h1>
-      <Counter />
+    <div className={"App " + unit}>
+    <select onChange={handleSelect}>
+      <option value="C"> C </option>
+      <option value="F"> F </option>
+    </select>
+    <div>
+      {weather.map(place=>
+       (<WeatherWidget
+        unit={unit}
+        city={place.city}
+        icon={place.icon} 
+        celsius={place.celsius} />) )}
+      </div>
+   </div>
+    
 
-      <select onChange={toggleTheme}>
+    /*<div className={"App " + theme}>
+      <h1>React - state and events</h1> 
+      <Counter />
+      <select onChange={handleSelect}>
         <option value="light"> Light </option>
         <option value="dark"> Dark </option>
       </select>
-
       <button onClick={() => setUnit("C")}> °C </button>
       <button onClick={() => setUnit("F")}> °F </button>
-
       <div>
         <WeatherWidget city="Miami" icon="🟡" celsius={29} unit={unit} />
         <WeatherWidget city="Mexico City" icon="⛈️" celsius={17} unit={unit} />
         <WeatherWidget city="Berlin" icon="🌥️" celsius={20} unit={unit} />
         <WeatherWidget city="Barcelona" icon="🌤️" celsius={28} unit={unit} />
       </div>
-    </div>
+    </div>  */
   );
 }
 
